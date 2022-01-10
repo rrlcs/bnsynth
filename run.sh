a66bae51dfcafd7f1bf12cb3640da35603181299
rrlcs <er.raviraja.rr@gmail.com>
2022-01-10 18:28:27 +0530

    commit

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python3 run.py \
 --th=0.5 \
 --P=$1 \
 --train=1 \
---tnorm_name=godel \
+--tnorm_name=product \
 --learning_rate=0.001 \
 --run_for_all_outputs=1 \
 --verilog_spec_location=verilog \
