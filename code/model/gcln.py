1d968d990a84140635b4a232643b1e6c0d0ed125
rrlcs <er.raviraja.rr@gmail.com>
2022-02-21 16:14:13 +0530

    updated gitignore

--- code/model/gcln.py
+++ code/model/gcln.py
@@ -25,14 +25,14 @@ class GCLN_CNF_Arch1(torch.nn.Module):
         self.layer_or_weights = torch.nn.Parameter(
             torch.Tensor(
                 self.input_size, K
-            ).uniform_(0.4, 0.6).to(dtype=torch.double).to(self.device)
+            ).uniform_(0., 1.).to(dtype=torch.double).to(self.device)
         )
         # self.layer_or_weights.data = torch.tensor([[1.0], [1.0]])
         # self.G2.shape: num_of_output_var * K x 1
         self.layer_and_weights = torch.nn.Parameter(
             torch.Tensor(
                 K, 1
-            ).uniform_(0.5, 0.6).to(dtype=torch.double).to(self.device)
+            ).uniform_(0., 1.).to(dtype=torch.double).to(self.device)
         )
         # self.layer_and_weights.data = torch.tensor([[1.0]])
         # self.b1.shape: 2 * no_input_var x K
