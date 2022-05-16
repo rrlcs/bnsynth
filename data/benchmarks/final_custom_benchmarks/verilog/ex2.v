55b5e5bffa63ec3157e7bf35062c212340672a6b
rrlcs <er.raviraja.rr@gmail.com>
2022-05-17 00:50:41 +0530

    miscel

--- data/benchmarks/final_custom_benchmarks/verilog/ex2.v
+++
@@ -1,6 +0,0 @@
-module formula(i_0,i_1, i_2,out);
-	input i_0 ,i_1, i_2;
-
-	output out;
-	assign out = i_1 & ~i_2;
-endmodule
--- data/benchmarks/final_custom_benchmarks/verilog/xnor_2_1.v
+++
@@ -1,6 +0,0 @@
-module formula(i_0,i_1,out);
-	input i_0 ,i_1;
-
-	output out;
-	assign out = ~(i_0 ^ i_1);
-endmodule
